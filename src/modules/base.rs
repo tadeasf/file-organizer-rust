4f4245bc8baa8e165d508e9df2d2dccaeedd4555
tadeasf <business@tadeasfort.com>
2025-03-25 20:54:09 +0100

    archive manager

--- src/modules/base.rs
+++ src/modules/base.rs
@@ -11,17 +11,21 @@ pub trait FileOrganizer {
     async fn run(&self) -> Result<()>;
     
     /// Whether the organizer operates recursively on subdirectories
+    #[allow(unused)]
     fn is_recursive(&self) -> bool;
     
     /// Get the input directory for the operation
+    #[allow(unused)]
     fn get_input_dir(&self) -> Option<&PathBuf>;
     
     /// Set the input directory for the operation
+    #[allow(unused)]
     fn set_input_dir(&mut self, dir: PathBuf);
     
     /// Process a single file
     fn process_file(&self, file: &PathBuf) -> Result<()>;
     
     /// Create necessary directories for the operation
+    #[allow(unused)]
     fn create_directories(&self, base_dir: &PathBuf) -> Result<()>;
 } 
